cf2e9943e57456b98b515c2a92787081fdb789af
Lennert Nijs <lennert.nijs@student.kuleuven.be>
2024-01-29 18:57:17 +0100

    tests added for faulty constructors

--- desktop/src/test/ItemTest/ToolTest.java
+++ desktop/src/test/ItemTest/ToolTest.java
@@ -16,4 +16,16 @@ public class ToolTest {
                 () -> Assertions.assertEquals(tool.getProficiency(), 1500)
         );
     }
+
+    @Test
+    public void testConstructorInvalid(){
+        Tool.Builder builder1 = Tool.toolBuilder().name(null).spritePath("pickaxe.png").proficiency(1500);
+        Tool.Builder builder2 = Tool.toolBuilder().name("Pickaxe").spritePath(null).proficiency(1500);
+        Tool.Builder builder3 = Tool.toolBuilder().name("Pickaxe").spritePath("pickaxe.png").proficiency(0);
+        Assertions.assertAll(
+                () -> Assertions.assertThrows(NullPointerException.class, builder1::build),
+                () -> Assertions.assertThrows(NullPointerException.class, builder2::build),
+                () -> Assertions.assertThrows(IllegalArgumentException.class, builder3::build)
+        );
+    }
 }
