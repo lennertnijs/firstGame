29731ba112053ec3353343e38f02e0dd7b479e79
Lennert Nijs <lennert.nijs@student.kuleuven.be>
2024-01-29 04:08:02 +0100

    tools

--- desktop/src/com/mygdx/game/Item/Tool.java
+++ desktop/src/com/mygdx/game/Item/Tool.java
@@ -2,17 +2,11 @@ package com.mygdx.game.Item;
 
 public class Tool extends UsableItem{
 
-    private final int durability;
     private final int proficiency;
 
     public Tool(Builder<?> builder){
         super(builder);
-        this.durability = builder.durability;
         this.proficiency = builder.proficiency;
-    }
-
-    public int getDurability(){
-        return durability;
     }
 
     public int getProficiency(){
@@ -20,13 +14,9 @@ public class Tool extends UsableItem{
     }
 
     public static class Builder<T extends Builder<T>> extends UsableItem.Builder<T>{
-        private int durability;
+
         private int proficiency;
 
-        public T durability(int durability){
-            this.durability = durability;
-            return self();
-        }
 
         public T proficiency(int proficiency){
             this.proficiency = proficiency;
@@ -35,9 +25,6 @@ public class Tool extends UsableItem{
 
         @Override
         public Tool build(){
-            if(durability <= 0){
-                throw new IllegalArgumentException("The durability of a tool has to be strictly positive");
-            }
             if(proficiency <= 0){
                 throw new IllegalArgumentException("The proficiency of a tool has to be strictly positive");
             }
--- desktop/src/test/ItemTest/ToolTest.java
+++ desktop/src/test/ItemTest/ToolTest.java
@@ -1,11 +1,13 @@
 package ItemTest;
 
+import com.mygdx.game.Item.Tool;
 import org.junit.jupiter.api.Test;
 
 public class ToolTest {
 
     @Test
     public void testConstructor(){
-
+        Tool.Builder<?> toolBuilder = new Tool.Builder<>();
+        toolBuilder.stackSize(1).name("Pickaxe").spritePath("tools/pickaxe.png").proficiency(1500).build();
     }
 }
