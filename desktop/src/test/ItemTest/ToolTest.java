d90d551e5fa15b029b90464e28ad94a78c4c010b
Lennert Nijs <lennert.nijs@student.kuleuven.be>
2024-01-29 04:13:06 +0100

    tools

--- desktop/src/test/ItemTest/ToolTest.java
+++ desktop/src/test/ItemTest/ToolTest.java
@@ -1,6 +1,7 @@
 package ItemTest;
 
 import com.mygdx.game.Item.Tool;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ToolTest {
@@ -8,6 +9,21 @@ public class ToolTest {
     @Test
     public void testConstructor(){
         Tool.Builder<?> toolBuilder = new Tool.Builder<>();
-        toolBuilder.stackSize(1).name("Pickaxe").spritePath("tools/pickaxe.png").proficiency(1500).build();
+        Tool tool = toolBuilder.stackSize(1).name("Pickaxe").spritePath("tools/pickaxe.png").proficiency(1500).build();
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(tool.getName(), "Pickaxe"),
+                () -> Assertions.assertEquals(tool.getSpritePath(), "tools/pickaxe.png"),
+                () -> Assertions.assertEquals(tool.getStackSize(), 1),
+                () -> Assertions.assertEquals(tool.getProficiency(), 1500)
+        );
+    }
+
+    @Test
+    public void testConstructorInvalid(){
+        Tool.Builder<?> toolBuilder = new Tool.Builder<>();
+        toolBuilder.stackSize(1).name("Pickaxe").spritePath("tools/pickaxe.png").proficiency(0);
+        Assertions.assertAll(
+                () -> Assertions.assertThrows(IllegalArgumentException.class, toolBuilder::build)
+        );
     }
 }
