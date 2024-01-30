73ec2347f962aed9146c8783f26514eb2b021b33
Lennert Nijs <lennert.nijs@student.kuleuven.be>
2024-01-30 12:25:22 +0100

    Stone.java basic structure added

--- desktop/src/com/mygdx/game/Breakable/Stone.java
+++ desktop/src/com/mygdx/game/Breakable/Stone.java
@@ -1,4 +1,81 @@
 package com.mygdx.game.Breakable;
 
+import com.mygdx.game.Entity.Position;
+
+import java.util.Objects;
+
 public class Stone {
+
+    private final Position position;
+    private float healthPoints;
+    private final int hardness;
+
+    public Stone(Builder builder){
+        position = builder.position;
+        healthPoints = builder.healthPoints;
+        hardness = builder.hardness;
+    }
+
+    public Position getPosition(){
+        return position;
+    }
+
+    public float getHealthPoints(){
+        return healthPoints;
+    }
+
+    public int getHardness(){
+        return hardness;
+    }
+
+    public void setHealthPoints(float newHealthPoints){
+        if(newHealthPoints <= 0 || newHealthPoints > healthPoints){
+            throw new IllegalArgumentException("The new health points is invalid");
+        }
+        this.healthPoints = newHealthPoints;
+    }
+
+
+
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder{
+
+        private Position position;
+        private float healthPoints;
+        private int hardness;
+
+        private Builder(){
+
+        }
+
+        public Builder position(Position position){
+            this.position = position;
+            return this;
+        }
+
+        public Builder healthPoints(float healthPoints){
+            this.healthPoints = healthPoints;
+            return this;
+        }
+
+        public Builder hardness(int hardness){
+            this.hardness = hardness;
+            return this;
+        }
+
+        public Stone build(){
+            Objects.requireNonNull(position, "Could not build the stone");
+            if(healthPoints <= 0){
+                throw new IllegalArgumentException("Could not build the stone");
+            }
+            if(hardness <= 0){
+                throw new IllegalArgumentException("Cannot build the stone");
+            }
+            return new Stone(this);
+        }
+    }
 }
