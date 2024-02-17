53aff1742aa40191ba4f342364de3329b8b57a8a
Lennert Nijs <lennert.nijs@student.kuleuven.be>
2024-02-17 18:26:18 +0100

    Added the most important methods to BreakableService

--- desktop/src/com/mygdx/game/Tree/BreakableService.java
+++ desktop/src/com/mygdx/game/Tree/BreakableService.java
@@ -4,7 +4,11 @@ import com.mygdx.game.Entity.Position;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import static com.mygdx.game.Constants.STONE_HEIGHT;
+import static com.mygdx.game.Constants.STONE_WIDTH;
 
 public class BreakableService {
 
@@ -55,5 +59,63 @@ public class BreakableService {
             }
         }
         return false;
+    }
+
+    public boolean hitBoxCollidesWithBreakable(Position position, int width, int height){
+        Objects.requireNonNull(position, "Cannot check breakable collision with a null hit box starting point");
+        if(width < 0 || height < 0){
+            throw new IllegalArgumentException("Cannot check breakable collision with a hit box of negative measures");
+        }
+        int x = position.getX();
+        int y = position.getY();
+        for(Breakable breakable : repository.getBreakables()){
+            int breakableX = breakable.getPosition().getX();
+            int breakableY = breakable.getPosition().getY();
+            int breakableWidth = breakable.getWidth();
+            int breakableHeight = breakable.getHeight();
+            boolean collides = x + width >= breakableX && x <= breakableX + breakableWidth &&
+                               y + height >= breakableY && y <= breakableY + breakableHeight;
+            if(collides){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Optional<Breakable> getBreakableCollidingWithPoint(Position position){
+        Objects.requireNonNull(position, "Cannot check breakable collision with a null point");
+        int x = position.getX();
+        int y = position.getY();
+        for(Breakable breakable: repository.getBreakables()){
+            int breakableX = breakable.getPosition().getX();
+            int breakableY = breakable.getPosition().getY();
+            boolean collides = x >= breakableX && x < breakableX + breakable.getWidth() &&
+                    y >= breakableY && y < breakableY + breakable.getHardness();
+            if(collides){
+                return Optional.of(breakable);
+            }
+        }
+        return null;
+    }
+
+    public Optional<Breakable> getBreakableCollidingWithHitBox(Position position, int width, int height){
+        Objects.requireNonNull(position, "Cannot check breakable collision with a null hit box starting point");
+        if(width < 0 || height < 0){
+            throw new IllegalArgumentException("Cannot check breakable collision with a hit box of negative measures");
+        }
+        int x = position.getX();
+        int y = position.getY();
+        for(Breakable breakable : repository.getBreakables()){
+            int breakableX = breakable.getPosition().getX();
+            int breakableY = breakable.getPosition().getY();
+            int breakableWidth = breakable.getWidth();
+            int breakableHeight = breakable.getHeight();
+            boolean collides = x + width >= breakableX && x <= breakableX + breakableWidth &&
+                    y + height >= breakableY && y <= breakableY + breakableHeight;
+            if(collides){
+                return Optional.of(breakable);
+            }
+        }
+        return null;
     }
 }
