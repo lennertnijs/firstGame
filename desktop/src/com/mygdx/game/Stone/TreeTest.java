4d46a9aa40e9c584cea98cc043b39721eb292d07
Lennert Nijs <lennert.nijs@student.kuleuven.be>
2024-02-17 16:34:31 +0100

    Wrote tests for tree.java

--- desktop/src/com/mygdx/game/Stone/TreeTest.java
+++ desktop/src/com/mygdx/game/Stone/TreeTest.java
@@ -58,4 +58,26 @@ public class TreeTest {
                 () -> Assertions.assertTrue(tree.isBroken())
         );
     }
+
+    @Test
+    public void testEqualsAndHashCode(){
+        Position position1 = Position.builder().x(500).y(500).build();
+        Position position2 = Position.builder().x(250).y(250).build();
+        Tree tree1 = Tree.create(position1, 250, 100);
+        Tree tree2 = Tree.create(position2, 250, 100);
+        Tree tree3 = Tree.create(position1, 250, 100);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(tree1, tree3),
+                () -> Assertions.assertEquals(tree1.hashCode(), tree3.hashCode()),
+
+                () -> Assertions.assertEquals(tree1, tree1),
+                () -> Assertions.assertEquals(tree1.hashCode(), tree1.hashCode()),
+
+                () -> Assertions.assertNotEquals(tree1, tree2),
+                () -> Assertions.assertNotEquals(tree1.hashCode(), tree2.hashCode()),
+
+                () -> Assertions.assertNotEquals(tree1, null),
+                () -> Assertions.assertNotEquals(tree1, new Object())
+        );
+    }
 }
