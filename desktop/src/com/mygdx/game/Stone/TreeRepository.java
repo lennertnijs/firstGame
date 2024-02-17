b3d641aee2ea12b34276da4d49ac70cfccf7a6cb
Lennert Nijs <lennert.nijs@student.kuleuven.be>
2024-02-17 16:47:17 +0100

    added TreeDAO to read in some tree data, and TreeRepository to save them

---
+++ desktop/src/com/mygdx/game/Stone/TreeRepository.java
@@ -0,0 +1,30 @@
+package com.mygdx.game.Stone;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class TreeRepository {
+
+    private final List<Tree> treeList;
+
+    protected TreeRepository(){
+        this.treeList = new TreeDAO().readTrees().stream().filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    protected List<Tree> getTrees(){
+        return this.treeList;
+    }
+
+    protected int getTreeAmount(){
+        return treeList.size();
+    }
+
+    protected void add(Tree tree){
+        treeList.add(tree);
+    }
+
+    protected void remove(Tree tree){
+        treeList.remove(tree);
+    }
+}
