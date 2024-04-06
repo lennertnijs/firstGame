75754f4b9a0804121b68bca66084894533b26d59
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-07 01:02:23 +0200

    Changed the tests a bit

--- desktop/src/test/V2/AnimationRepositoryTest.java
+++ desktop/src/test/V2/AnimationRepositoryTest.java
@@ -21,63 +21,55 @@ public class AnimationRepositoryTest {
 
     private Animation<Integer> animation1;
     private Animation<Integer> animation2;
-    private Animation<Integer> animation3;
-    private Map<Direction, Animation<Integer>> map1;
-    private DirectionAnimationRepo<Integer> repo1;
-    private DirectionAnimationRepo<Integer> repo2;
+    private DirectionAnimationRepo<Integer> runningDirectionRepo;
     private AnimationRepo<Integer> animationRepo;
 
     @BeforeEach
     public void initialise(){
         animation1 = new Animation<>(new Integer[]{1,2,3,4,5,6}, 3);
         animation2 = new Animation<>(new Integer[]{1,2,3}, 3);
-        animation3 = new Animation<>(new Integer[]{1,2,3,4,5,6}, 3);
+        Animation<Integer> animation3 = new Animation<>(new Integer[]{4, 5, 6}, 3);
 
-        map1 = new LinkedHashMap<Direction, Animation<Integer>>(){{
-            put(Direction.UP, animation1);
-            put(Direction.RIGHT, animation2);
-            put(Direction.DOWN, animation3);
-        }};
+        Map<Direction, Animation<Integer>> runningMap = new LinkedHashMap<>();
+        runningMap.put(Direction.UP, animation1);
+        runningMap.put(Direction.RIGHT, animation2);
+        runningMap.put(Direction.DOWN, animation3);
+        runningDirectionRepo = new DirectionAnimationRepo<>(runningMap);
 
-        Map<Direction, Animation<Integer>> map2 = new LinkedHashMap<Direction, Animation<Integer>>(){{
-            put(Direction.UP, animation1);
-        }};
+        Map<Direction, Animation<Integer>> walkingMap = new LinkedHashMap<Direction, Animation<Integer>>();
+        walkingMap.put(Direction.UP, animation1);
+        DirectionAnimationRepo<Integer> walkingDirectionRepo = new DirectionAnimationRepo<>(walkingMap);
 
-        repo1 = new DirectionAnimationRepo<>(map1);
-        repo2 = new DirectionAnimationRepo<>(map2);
-
-        Map<ActivityType, IDirectionAnimationRepository<Integer>> repoMap =
-                new HashMap<ActivityType, IDirectionAnimationRepository<Integer>>(){{
-            put(ActivityType.RUNNING, repo1);
-            put(ActivityType.WALKING, repo2);
-        }};
+        Map<ActivityType, IDirectionAnimationRepository<Integer>> repoMap = new HashMap<>();
+        repoMap.put(ActivityType.RUNNING, runningDirectionRepo);
+        repoMap.put(ActivityType.WALKING, walkingDirectionRepo);
         animationRepo = new AnimationRepo<>(repoMap);
     }
 
     @Test
-    public void testConstructorInvalid(){
+    public void testConstructorWithNull(){
         assertThrows(NullPointerException.class, () -> new AnimationRepo<>(null));
-        Map<ActivityType, IDirectionAnimationRepository<Integer>> map1 = new HashMap<ActivityType, IDirectionAnimationRepository<Integer>>(){{
-            put(null, repo1);
-        }};
-        assertThrows(NullPointerException.class, () -> new AnimationRepo<>(map1));
-        Map<ActivityType, IDirectionAnimationRepository<Integer>> map2 = new HashMap<ActivityType, IDirectionAnimationRepository<Integer>>(){{
-            put(ActivityType.IDLING, null);
-        }};
-        assertThrows(NullPointerException.class, () -> new AnimationRepo<>(map2));
+    }
+
+    @Test
+    public void testConstructorWithNullKey(){
+        Map<ActivityType, IDirectionAnimationRepository<Integer>> nullKeyMap = new HashMap<>();
+        nullKeyMap.put(null, runningDirectionRepo);
+        assertThrows(NullPointerException.class, () -> new AnimationRepo<>(nullKeyMap));
+    }
+    @Test
+    public void testConstructorWithNullValue(){
+        Map<ActivityType, IDirectionAnimationRepository<Integer>> nullValueMap = new HashMap<>();
+        nullValueMap.put(ActivityType.IDLING, null);
+        assertThrows(NullPointerException.class, () -> new AnimationRepo<>(nullValueMap));
     }
 
     @Test
     public void testGetAnimation(){
-        assertEquals(animationRepo.getAnimation(ActivityType.WALKING, Direction.UP), new Animation<>(new Integer[]{1,2,3,4,5,6}, 3));
-        assertEquals(animationRepo.getAnimation(ActivityType.RUNNING, Direction.UP), new Animation<>(new Integer[]{1,2,3,4,5,6}, 3));
-        assertEquals(animationRepo.getAnimation(ActivityType.RUNNING, Direction.RIGHT), new Animation<>(new Integer[]{1,2,3}, 3));
+        assertEquals(animationRepo.getAnimation(ActivityType.WALKING, Direction.UP), animation1);
+        assertEquals(animationRepo.getAnimation(ActivityType.RUNNING, Direction.UP), animation1);
+        assertEquals(animationRepo.getAnimation(ActivityType.RUNNING, Direction.RIGHT), animation2);
+        assertThrows(NoSuchElementException.class,
+                () -> animationRepo.getAnimation(ActivityType.IDLING, Direction.DOWN));
     }
-
-    @Test
-    public void testGetAnimationNoMappingForActivity(){
-        assertThrows(NoSuchElementException.class, () -> animationRepo.getAnimation(ActivityType.IDLING, Direction.DOWN));
-    }
-
-
 }
