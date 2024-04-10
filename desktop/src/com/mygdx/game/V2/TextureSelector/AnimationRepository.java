d55009a25956193ed5a4347e0879f012e5b871b0
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-10 10:28:12 +0300

    hotswapped Animation for IAnimation

--- desktop/src/com/mygdx/game/V2/TextureSelector/AnimationRepository.java
+++ desktop/src/com/mygdx/game/V2/TextureSelector/AnimationRepository.java
@@ -6,20 +6,20 @@ import java.util.Objects;
 
 public final class AnimationRepository<T> {
 
-    private final Map<AnimationKey, Animation<T>> mapping;
+    private final Map<AnimationKey, IAnimation<T>> mapping;
 
-    public AnimationRepository(Map<AnimationKey, Animation<T>> map){
+    public AnimationRepository(Map<AnimationKey, IAnimation<T>> map){
         validateMap(map);
         this.mapping = map;
     }
 
-    private void validateMap(Map<AnimationKey, Animation<T>> map){
+    private void validateMap(Map<AnimationKey, IAnimation<T>> map){
         Objects.requireNonNull(map, "Cannot make a AnimationRepository from null.");
         if(map.containsKey(null) || map.containsValue(null))
             throw new NullPointerException("Cannot make an AnimationRepository with a null key or value.");
     }
 
-    public Animation<T> getAnimation(AnimationKey key){
+    public IAnimation<T> getAnimation(AnimationKey key){
         Objects.requireNonNull(key, "Cannot retrieve an Animation for a null AnimationKey.");
         if(!mapping.containsKey(key))
             throw new NoSuchElementException("No Animation for the given key exists.");
