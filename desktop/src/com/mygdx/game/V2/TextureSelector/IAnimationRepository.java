ab037e38659d1faa0176649fc7905b0ddc610fcc
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-10 10:31:26 +0300

    Replaced the old animation repo with the new better one. TextureSelector tests to be written

--- desktop/src/com/mygdx/game/V2/TextureSelector/IAnimationRepository.java
+++ desktop/src/com/mygdx/game/V2/TextureSelector/IAnimationRepository.java
@@ -1,9 +1,6 @@
 package com.mygdx.game.V2.TextureSelector;
 
-import com.mygdx.game.V2.ActivityType;
-import com.mygdx.game.V2.Direction;
-
 public interface IAnimationRepository<T> {
 
-    Animation<T> getAnimation(ActivityType type, Direction direction);
+    Animation<T> getAnimation(AnimationKey key);
 }
