54d51e3b78570f964e16c9365642ca54f176ff8e
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-22 22:47:06 +0200

    uncommented the commented path finder

--- desktop/src/com/mygdx/game/V2/BFSPathFinder.java
+++ desktop/src/com/mygdx/game/V2/BFSPathFinder.java
@@ -12,28 +12,28 @@ public class BFSPathFinder<T> implements PathFinderStrategy<T>{
 
     @Override
     public List<T> findPath(T start, T goal, IGraph<T> graph) {
-//        Objects.requireNonNull(start, "Cannot find a path using BFS starting in null.");
-//        Objects.requireNonNull(goal, "Cannot find a path using BFS ending in null.");
-//        Objects.requireNonNull(graph, "Cannot find a path using BFS over a null network.");
-//        LinkedList<List<T>> queue = new LinkedList<>();
-//        queue.add(Collections.singletonList(start));
-//        while(!queue.isEmpty()){
-//            List<T> currentPath = queue.pop();
-//            T currentMapPosition = currentPath.get(currentPath.size() - 1);
-//            if(!graph.hasVertex(currentMapPosition))
-//                throw new NoSuchElementException("No mapping for the given MapPosition was found in the network.");
-//            for(T t : graph.getNeighbors(currentMapPosition)){
-//                List<T> clonedPath = new ArrayList<>(currentPath);
-//                boolean visited = clonedPath.contains(t);
-//                if(!visited){
-//                    clonedPath.add(t);
-//                    if(t.equals(goal))
-//                        return clonedPath;
-//                    else
-//                        queue.addLast(clonedPath);
-//                }
-//            }
-//        }
+        Objects.requireNonNull(start, "Cannot find a path using BFS starting in null.");
+        Objects.requireNonNull(goal, "Cannot find a path using BFS ending in null.");
+        Objects.requireNonNull(graph, "Cannot find a path using BFS over a null network.");
+        LinkedList<List<T>> queue = new LinkedList<>();
+        queue.add(Collections.singletonList(start));
+        while(!queue.isEmpty()){
+            List<T> currentPath = queue.pop();
+            T currentMapPosition = currentPath.get(currentPath.size() - 1);
+            if(!graph.hasVertex(currentMapPosition))
+                throw new NoSuchElementException("No mapping for the given MapPosition was found in the network.");
+            for(T t : graph.getNeighbors(currentMapPosition)){
+                List<T> clonedPath = new ArrayList<>(currentPath);
+                boolean visited = clonedPath.contains(t);
+                if(!visited){
+                    clonedPath.add(t);
+                    if(t.equals(goal))
+                        return clonedPath;
+                    else
+                        queue.addLast(clonedPath);
+                }
+            }
+        }
         throw new NoSuchElementException("No path was found to the goal.");
     }
 }
--- desktop/src/test/V2/BFSPathFinderTest.java
+++ desktop/src/test/V2/BFSPathFinderTest.java
@@ -6,6 +6,11 @@ import com.mygdx.game.V2.Graph.IGraph;
 import com.mygdx.game.V2.WeekSchedule.Location;
 import com.mygdx.game.V2.Util.Position;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -32,36 +37,31 @@ public class BFSPathFinderTest {
         mp6 = Location.create("map2", Position.create(0,0));
         mp7 = Location.create("map2", Position.create(50,0));
 
-//        graph.addVertices(new ArrayList<>(Arrays.asList(mp1, mp2, mp3, mp4, mp5, mp6, mp7)));
-//        graph.addEdges(mp1, new ArrayList<>(Arrays.asList(mp2, mp3)));
-//        graph.addEdges(mp2, new ArrayList<>(Arrays.asList(mp1, mp4)));
-//        graph.addEdges(mp3, new ArrayList<>(Arrays.asList(mp1, mp4)));
-//        graph.addEdges(mp4, new ArrayList<>(Arrays.asList(mp2, mp3, mp5)));
-//        graph.addEdges(mp5, new ArrayList<>(Arrays.asList(mp4, mp6)));
-//        graph.addEdges(mp6, new ArrayList<>(Arrays.asList(mp5, mp7)));
+        graph.addVertices(new ArrayList<>(Arrays.asList(mp1, mp2, mp3, mp4, mp5, mp6, mp7)));
+        graph.addEdges(mp1, new ArrayList<>(Arrays.asList(mp2, mp3)));
+        graph.addEdges(mp2, new ArrayList<>(Arrays.asList(mp1, mp4)));
+        graph.addEdges(mp3, new ArrayList<>(Arrays.asList(mp1, mp4)));
+        graph.addEdges(mp4, new ArrayList<>(Arrays.asList(mp2, mp3, mp5)));
+        graph.addEdges(mp5, new ArrayList<>(Arrays.asList(mp4, mp6)));
+        graph.addEdges(mp6, new ArrayList<>(Arrays.asList(mp5, mp7)));
     }
 
-//    @Test
-//    public void testFindPath1(){
-//        assertEquals(bfsPathFinder.findPath(mp1, mp2, graph), new ArrayList<>(Arrays.asList(mp1, mp2)));
-//    }
-//
-//    @Test
-//    public void testFindPath2(){
-//        assertEquals(bfsPathFinder.findPath(mp1, mp7, graph), new ArrayList<>(Arrays.asList(mp1, mp2, mp4, mp5, mp6, mp7)));
-//    }
-//
-//    @Test
-//    public void testFindPathWithNull(){
-//        assertThrows(NullPointerException.class, () -> bfsPathFinder.findPath(null, mp1, graph));
-//        assertThrows(NullPointerException.class, () -> bfsPathFinder.findPath(mp1, null, graph));
-//        assertThrows(NullPointerException.class, () -> bfsPathFinder.findPath(mp1, mp2, null));
-//    }
-//
-//    @Test
-//    public void testFindPathNoSuchElement(){
-//        Location noSuch = Location.create("map3", Position.create(0,0));
-//        assertThrows(NoSuchElementException.class, () -> bfsPathFinder.findPath(noSuch, mp1, graph));
-//        assertThrows(NoSuchElementException.class, () -> bfsPathFinder.findPath(mp1, noSuch, graph));
-//    }
+    @Test
+    public void testFindPath1(){
+        assertEquals(bfsPathFinder.findPath(mp1, mp2, graph), new ArrayList<>(Arrays.asList(mp1, mp2)));
+    }
+
+    @Test
+    public void testFindPathWithNull(){
+        assertThrows(NullPointerException.class, () -> bfsPathFinder.findPath(null, mp1, graph));
+        assertThrows(NullPointerException.class, () -> bfsPathFinder.findPath(mp1, null, graph));
+        assertThrows(NullPointerException.class, () -> bfsPathFinder.findPath(mp1, mp2, null));
+    }
+
+    @Test
+    public void testFindPathNoSuchElement(){
+        Location noSuch = Location.create("map3", Position.create(0,0));
+        assertThrows(NoSuchElementException.class, () -> bfsPathFinder.findPath(noSuch, mp1, graph));
+        assertThrows(NoSuchElementException.class, () -> bfsPathFinder.findPath(mp1, noSuch, graph));
+    }
 }
