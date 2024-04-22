269197356419bd583d003340330566538ce6f570
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-23 00:10:18 +0200

    uncommented the commented path finder

--- desktop/src/com/mygdx/game/V2/Character.java
+++ desktop/src/com/mygdx/game/V2/Character.java
@@ -1,6 +1,11 @@
 package com.mygdx.game.V2;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.V2.TextureSelector.ITextureSelector;
+import com.mygdx.game.V2.Util.ActivityType;
+import com.mygdx.game.V2.Util.Direction;
+
+import java.util.Objects;
 
 public abstract class Character {
 
@@ -10,5 +15,23 @@ public abstract class Character {
     public Character(String name, ITextureSelector textureSelector){
         this.name = name;
         this.textureSelector = textureSelector;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Texture getTexture(){
+        return textureSelector.getTexture();
+    }
+
+    public void setDirection(Direction direction){
+        Objects.requireNonNull(direction, "Direction is null.");
+        textureSelector.setDirection(direction);
+    }
+
+    public void setActivity(ActivityType activityType){
+        Objects.requireNonNull(activityType, "Activity type is null.");
+        textureSelector.setActivityType(activityType);
     }
 }
