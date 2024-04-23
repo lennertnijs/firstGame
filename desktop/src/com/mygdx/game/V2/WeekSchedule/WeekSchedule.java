174a0491bd50221a5dbfb30db7f5e99ff7bc9ca1
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-24 00:58:00 +0200

    Made a WeekSchedule.java interface!!

--- desktop/src/com/mygdx/game/V2/WeekSchedule/WeekSchedule.java
+++ desktop/src/com/mygdx/game/V2/WeekSchedule/WeekSchedule.java
@@ -1,53 +1,110 @@
 package com.mygdx.game.V2.WeekSchedule;
 
 import com.mygdx.game.V2.Util.Day;
+import com.mygdx.game.V2.Util.Time;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
+/**
+ * Represents a week schedule.
+ * A week schedule maps days to schedules.
+ * IMMUTABLE
+ */
 public final class WeekSchedule {
 
-    private final Map<Day, Schedule> daySchedules;
+    /**
+     * The schedules, mapped onto by their relevant day.
+     */
+    private final Map<Day, Schedule> schedules;
 
-    private WeekSchedule(Map<Day, Schedule> daySchedules){
-        this.daySchedules = daySchedules;
+    /**
+     * Creates a new immutable {@link WeekSchedule}.
+     * @param schedules The map of {@link Day}s and their {@link Schedule}s. Cannot be null. Cannot contain a null.
+     */
+    public WeekSchedule(Map<Day, Schedule> schedules){
+        Objects.requireNonNull(schedules, "Map is null.");
+        if(schedules.containsKey(null))
+            throw new NullPointerException("Map contains null key.");
+        if(schedules.containsValue(null))
+            throw new NullPointerException("Map contains null value.");
+        this.schedules = new HashMap<>(schedules);
     }
 
-    public static WeekSchedule create(Map<Day, Schedule> daySchedules){
-        Objects.requireNonNull(daySchedules, "Cannot create a WeekSchedule with null.");
-        if(daySchedules.containsKey(null) || daySchedules.containsValue(null))
-            throw new NullPointerException("Cannot create a WeekSchedule with a null Day or DaySchedule.");
-        return new WeekSchedule(daySchedules);
+    /**
+     * {@inheritDoc}
+     */
+    public Map<Day, Schedule> schedules(){
+        return new HashMap<>(schedules);
     }
 
-    public Map<Day, Schedule> getDaySchedulesWithDays(){
-        return daySchedules;
+    /**
+     * {@inheritDoc}
+     */
+    public Schedule getSchedule(Day day){
+        validateDay(day);
+        return schedules.get(day);
     }
 
-    public Schedule getDaySchedule(Day day){
-        Objects.requireNonNull(day, "Cannot fetch a DaySchedule for null.");
-        if(!daySchedules.containsKey(day))
-            throw new NoSuchElementException("No DaySchedule exists for this Day.");
-        return daySchedules.get(day);
+    /**
+     * {@inheritDoc}
+     */
+    public boolean hasActivity(Day day, Time time){
+        validateDay(day);
+        Objects.requireNonNull(time, "Time is null.");
+        return schedules.get(day).hasActivityAt(time);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public Activity getActivity(Day day, Time time){
+        validateDay(day);
+        Objects.requireNonNull(time, "Time is null.");
+        return schedules.get(day).getActivityAt(time);
+    }
+
+    /**
+     * Validates the given {@link Day}.
+     * @param day The {@link Day}. Cannot be null.
+     *
+     * @throws NoSuchElementException If no mapping for the given {@link Day} exists.
+     */
+    private void validateDay(Day day){
+        Objects.requireNonNull(day, "Day is null.");
+        if(!schedules.containsKey(day))
+            throw new NoSuchElementException("No Schedule found for this Day.");
+    }
+
+    /**
+     * Compares this {@link WeekSchedule} to the given object and returns true if they're equal. Returns false otherwise.
+     * Two {@link WeekSchedule}s are the same if they have the same (key, value) pairs.
+     *
+     * @return True if equal. False otherwise.
+     */
     @Override
     public boolean equals(Object other){
-        if(!(other instanceof WeekSchedule)){
+        if(!(other instanceof WeekSchedule))
             return false;
-        }
         WeekSchedule schedule = (WeekSchedule) other;
-        return daySchedules.equals(schedule.daySchedules);
+        return schedules.equals(schedule.schedules);
     }
 
+    /**
+     * @return The hash code of this {@link WeekSchedule}.
+     */
     @Override
     public int hashCode(){
-        return daySchedules.hashCode();
+        return schedules.hashCode();
     }
 
+    /**
+     * @return THe string representation of this {@link WeekSchedule}.
+     */
     @Override
     public String toString(){
-        return String.format("WeekSchedule[DaySchedules=%s]", daySchedules);
+        return String.format("WeekSchedule[Schedules=%s]", schedules);
     }
 }
