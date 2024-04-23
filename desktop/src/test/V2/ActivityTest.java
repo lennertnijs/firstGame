5de1359d2fa2b70dd978afce212e38c011ecf84d
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-23 20:04:05 +0200

    ActivityTest done.

--- desktop/src/test/V2/ActivityTest.java
+++ desktop/src/test/V2/ActivityTest.java
@@ -25,4 +25,80 @@ public class ActivityTest {
 
         activity = new Activity(location, time, activityType);
     }
+
+    @Test
+    public void testConstructorWithNullLocation(){
+        assertThrows(NullPointerException.class, () -> new Activity(null, time, activityType));
+    }
+
+    @Test
+    public void testConstructorWithNullTime(){
+        assertThrows(NullPointerException.class, () -> new Activity(location, null, activityType));
+    }
+
+    @Test
+    public void testConstructorWithNullActivityType(){
+        assertThrows(NullPointerException.class, () -> new Activity(location, time, null));
+    }
+
+    @Test
+    public void testLocation(){
+        assertEquals(location, activity.location());
+    }
+
+    @Test
+    public void testTime(){
+        assertEquals(time, activity.time());
+    }
+
+    @Test
+    public void testActivityType(){
+        assertEquals(activityType, activity.type());
+    }
+
+    @Test
+    public void testEquals(){
+        Activity activity1 = new Activity(location, time, activityType);
+        Activity activity2 = new Activity(location, time, activityType);
+        Activity activity3 = new Activity(location, time, activityType);
+        Activity diffActivity = new Activity(location, new Time(0, 15), activityType);
+        // reflexive
+        assertEquals(activity1, activity1);
+        // symmetrical
+        assertEquals(activity1, activity2);
+        assertEquals(activity2, activity1);
+        // transitive
+        assertEquals(activity1, activity2);
+        assertEquals(activity2, activity3);
+        assertEquals(activity1, activity3);
+        // not equals
+        assertNotEquals(activity1, diffActivity);
+        assertNotEquals(activity1, new Object());
+        assertNotEquals(activity1, null);
+    }
+
+    @Test
+    public void testHashCode(){
+        Activity activity1 = new Activity(location, time, activityType);
+        Activity activity2 = new Activity(location, time, activityType);
+        Activity activity3 = new Activity(location, time, activityType);
+        Activity diffActivity = new Activity(location, new Time(0, 15), activityType);
+        // reflexive
+        assertEquals(activity1.hashCode(), activity1.hashCode());
+        // symmetrical
+        assertEquals(activity1.hashCode(), activity2.hashCode());
+        assertEquals(activity2.hashCode(), activity1.hashCode());
+        // transitive
+        assertEquals(activity1.hashCode(), activity2.hashCode());
+        assertEquals(activity2.hashCode(), activity3.hashCode());
+        assertEquals(activity1.hashCode(), activity3.hashCode());
+        // not equals
+        assertNotEquals(activity1.hashCode(), diffActivity.hashCode());
+    }
+
+    @Test
+    public void testToString(){
+        String expected = "Activity[Location[mapName=Map, position=Point[x=5, y=10]], Time[hours=10, minutes=15], Type=RUNNING]";
+        assertEquals(expected, activity.toString());
+    }
 }
