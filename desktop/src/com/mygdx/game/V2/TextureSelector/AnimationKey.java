7b4db5d287f1cd124b0bccbff8c109459c170ded
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-24 15:41:49 +0200

    AnimationKey documented

--- desktop/src/com/mygdx/game/V2/TextureSelector/AnimationKey.java
+++ desktop/src/com/mygdx/game/V2/TextureSelector/AnimationKey.java
@@ -5,11 +5,27 @@ import com.mygdx.game.V2.Util.Direction;
 
 import java.util.Objects;
 
+/**
+ * Represents a key for animation retrieval.
+ * A key holds an activity type and a direction.
+ * IMMUTABLE
+ */
 public final class AnimationKey {
 
+    /**
+     * The activity type.
+     */
     private final ActivityType activityType;
+    /**
+     * The direction.
+     */
     private final Direction direction;
 
+    /**
+     * Creates a new immutable {@link AnimationKey}.
+     * @param activityType The {@link ActivityType}. Cannot be null.
+     * @param direction The {@link Direction}. Cannot be null.
+     */
     public AnimationKey(ActivityType activityType, Direction direction){
         Objects.requireNonNull(activityType, "Activity type is null.");
         Objects.requireNonNull(direction, "Direction is null.");
@@ -17,14 +33,26 @@ public final class AnimationKey {
         this.direction = direction;
     }
 
+    /**
+     * @return The {@link ActivityType}.
+     */
     public ActivityType activityType(){
         return activityType;
     }
 
+    /**
+     * @return The {@link Direction}.
+     */
     public Direction direction(){
         return direction;
     }
 
+    /**
+     * Compares this {@link AnimationKey} to the given object and returns true if they're equal. Returns false otherwise.
+     * Two {@link AnimationKey}s are equal if they have the same activity type and direction.
+     *
+     * @return True if equal. False otherwise.
+     */
     @Override
     public boolean equals(Object other){
         if(!(other instanceof AnimationKey))
@@ -33,6 +61,9 @@ public final class AnimationKey {
         return activityType == key.activityType && direction == key.direction;
     }
 
+    /**
+     * @return The hash code of this {@link AnimationKey}.
+     */
     @Override
     public int hashCode(){
         int result = activityType.hashCode();
@@ -40,6 +71,9 @@ public final class AnimationKey {
         return result;
     }
 
+    /**
+     * @return The string representation of this {@link AnimationKey}.
+     */
     @Override
     public String toString(){
         return String.format("AnimationKey[ActivityType=%s, Direction=%s]", activityType, direction);
