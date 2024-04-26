faf2a8cf78c1621e3675d45dd0d3028887407ac6
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-26 21:01:00 +0200

    BFS done.

--- desktop/src/com/mygdx/game/V2/Navigation/BFSPathFinder.java
+++ desktop/src/com/mygdx/game/V2/Navigation/BFSPathFinder.java
@@ -2,15 +2,31 @@ package com.mygdx.game.V2.Navigation;
 
 import java.util.*;
 
+/**
+ * Represents a pathfinder using Bread-first search.
+ */
 public class BFSPathFinder<T> implements PathFinderStrategy<T> {
 
+    /**
+     * The graph.
+     */
     private final IGraph<T> graph;
 
+    /**
+     * Creates a new, semi-immutable {@link BFSPathFinder}.
+     * Note: it's semi-immutable because it copies the {@link IGraph}, but this means it relies on the immutability
+     * of the objects in the {@link IGraph}.
+     * @param graph The {@link IGraph}. Cannot be null.
+     */
     public BFSPathFinder(IGraph<T> graph){
         Objects.requireNonNull(graph, "Graph is null.");
         this.graph = graph.copy();
     }
 
+    /**
+     * {@inheritDoc}
+     * Finds one of the shortest paths (in vertices).
+     */
     @Override
     public List<T> findPath(T start, T goal) {
         Objects.requireNonNull(start, "Start is null.");
@@ -20,6 +36,14 @@ public class BFSPathFinder<T> implements PathFinderStrategy<T> {
         return BFS(start, goal);
     }
 
+    /**
+     * Finds one of the shortest paths (in vertices) between start and goal, in the graph.
+     * @param start The start object. Cannot be null.
+     * @param goal The end object. Cannot be null.
+     *
+     * @return The path.
+     * @throws NoSuchElementException If no path was found.
+     */
     private List<T> BFS(T start, T goal){
         Queue<List<T>> queue = new LinkedList<>();
         queue.add(new LinkedList<>(Collections.singletonList(start)));
