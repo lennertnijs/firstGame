5fec7bfdff7db83c956cf2d9507dc63dbfe7ba5d
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-29 01:05:21 +0200

    removed unused imports.

--- desktop/src/com/mygdx/game/V2/NPC.java
+++ desktop/src/com/mygdx/game/V2/NPC.java
@@ -1,10 +1,11 @@
 package com.mygdx.game.V2;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mygdx.game.V2.Dialogue.IDialogueData;
 import com.mygdx.game.V2.Navigation.INavigationData;
-import com.mygdx.game.V2.Navigation.NavigationData;
 import com.mygdx.game.V2.TextureSelector.ITextureSelector;
 import com.mygdx.game.V2.Util.Day;
+import com.mygdx.game.V2.Util.Location;
 import com.mygdx.game.V2.Util.Time;
 import com.mygdx.game.V2.WeekSchedule.Activity;
 import com.mygdx.game.V2.WeekSchedule.IWeekSchedule;
@@ -28,9 +29,19 @@ public final class NPC{
         this.stats = stats;
     }
 
-    public void update(Day day, Time time){
+    public void update(Day day, Time time, int delta){
         if(weekSchedule.hasActivity(day, time)){
             Activity activity = weekSchedule.getActivity(day, time);
+            graph.calculateAndStoreRoute(null, activity.location()); // should hold THIS.location
         }
+        if(graph.isMoving()){
+            Location next = graph.calculateNextLocation(null, delta * stats.getSpeed());
+            // set this location to the next one
+        }
+    }
+
+    public void updateTexture(){
+        TextureRegion t = selector.getTexture();
+        // set this sprite's texture to the given.
     }
 }
