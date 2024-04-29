930004f8821c7a9344c4aa2564a8b8793c9541c9
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-30 00:45:39 +0200

    Just wrote a bit of code in NPC to see if it makes some sense.

---
+++ desktop/src/com/mygdx/game/V2/General/Movable.java
@@ -0,0 +1,6 @@
+package com.mygdx.game.V2.General;
+
+public interface Movable {
+
+    void move(int deltaInMillis);
+}
--- desktop/src/com/mygdx/game/V2/NPC.java
+++ desktop/src/com/mygdx/game/V2/NPC.java
@@ -1,8 +1,8 @@
 package com.mygdx.game.V2;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mygdx.game.V2.Dialogue.IDialogueData;
 import com.mygdx.game.V2.General.GameObject;
+import com.mygdx.game.V2.General.Movable;
 import com.mygdx.game.V2.General.Sprite;
 import com.mygdx.game.V2.Navigation.INavigationData;
 import com.mygdx.game.V2.TextureSelector.ITextureSelector;
@@ -12,39 +12,50 @@ import com.mygdx.game.V2.Util.Time;
 import com.mygdx.game.V2.WeekSchedule.Activity;
 import com.mygdx.game.V2.WeekSchedule.IWeekSchedule;
 
-public final class NPC extends GameObject {
+public final class NPC extends GameObject implements Movable {
 
     private final String name;
     private final ITextureSelector selector;
-    private final INavigationData graph;
+    private final INavigationData navigationData;
     private final IWeekSchedule weekSchedule;
     private final IDialogueData dialogueData;
     private final NPCStats stats;
 
     private NPC(Sprite sprite, String name, ITextureSelector selector, IWeekSchedule weekSchedule,
-                INavigationData graph, IDialogueData dialogueData, NPCStats stats){
+                INavigationData navigationData, IDialogueData dialogueData, NPCStats stats){
         super(sprite);
         this.name = name;
         this.selector = selector;
         this.weekSchedule = weekSchedule;
-        this.graph = graph;
+        this.navigationData = navigationData;
         this.dialogueData = dialogueData;
         this.stats = stats;
     }
 
-    public void update(Day day, Time time, int delta){
-        if(weekSchedule.hasActivity(day, time)){
-            Activity activity = weekSchedule.getActivity(day, time);
-            graph.calculateAndStoreRoute(getCurrentLocation(), activity.location());
-        }
-        if(graph.isMoving()){
-            Location next = graph.calculateNextLocation(getCurrentLocation(), delta * stats.getSpeed());
-            setLocation(next);
-        }
+    public String getName(){
+        return name;
+    }
+
+    public void update(Day day, Time time){
+        if(!weekSchedule.hasActivity(day, time))
+            return;
+        Activity activity = weekSchedule.getActivity(day, time);
+        navigationData.calculateAndStoreRoute(getCurrentLocation(), activity.location());
+    }
+
+    public void move(int deltaInMillis){
+        if(!navigationData.isMoving())
+            return;
+        int movement = deltaInMillis * stats.getSpeed();
+        Location next = navigationData.calculateNextLocation(getCurrentLocation(), movement);
+        setLocation(next);
     }
 
     public void updateTexture(){
-        TextureRegion t = selector.getTexture();
-        setTexture(t);
+        setTexture(selector.getTexture());
+    }
+
+    public void handleInputLine(String line){
+        dialogueData.processInput(line);
     }
 }
