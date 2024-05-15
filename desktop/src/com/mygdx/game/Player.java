a8b4555c75d14ae147263b1f8a269fe52abf39ce
lennertnijs <lennert.nijs2001@icloud.com>
2024-05-15 20:53:26 +0200

    Quickly added a field.

--- desktop/src/com/mygdx/game/Player.java
+++ desktop/src/com/mygdx/game/Player.java
@@ -3,16 +3,19 @@ package com.mygdx.game;
 import com.mygdx.game.General.GameObject;
 import com.mygdx.game.General.Sprite;
 import com.mygdx.game.Inventory.IInventoryManager;
+import com.mygdx.game.TextureSelector.IAnimationManager;
 
 public final class Player extends GameObject{
 
 
     private final String name;
+    private final IAnimationManager animationManager;
     private final IInventoryManager inventoryManager;
     // add player stats
-    public Player(String name, Sprite sprite, IInventoryManager inventoryManager){
+    public Player(String name, Sprite sprite, IAnimationManager animationManager, IInventoryManager inventoryManager){
         super(sprite);
         this.name = name;
+        this.animationManager = animationManager;
         this.inventoryManager = inventoryManager;
     }
 }
