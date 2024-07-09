951004a3a7aff54dda7c7df5f588e1ee47dfe823
lennertnijs <lennert.nijs2001@icloud.com>
2024-07-09 13:43:53 +0200

    Removed rat, for now.

--- desktop/src/com/mygdx/game/Rat/RatState.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game.Rat;
-
-import com.mygdx.game.Bat.MonsterData;
-import com.mygdx.game.Util.Point;
-
-public interface RatState {
-
-    void move(MonsterData data, Rat rat);
-}
