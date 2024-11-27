12dfb26ca58abb9e07479e7c4f6fa9fe674e3be6
lennertnijs <lennert.nijs2001@icloud.com>
2024-11-27 22:12:19 +0100

    r tree code

--- desktop/src/com/mygdx/game/r_tree/R_tree.java
+++ desktop/src/com/mygdx/game/r_tree/R_tree.java
@@ -3,7 +3,6 @@ package com.mygdx.game.r_tree;
 import com.mygdx.game.util.Vec2;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public final class R_tree<T extends GameObject2D> {
 
@@ -174,11 +173,7 @@ public final class R_tree<T extends GameObject2D> {
     }
 
     /**
-     * Attempts all possible splits, and picks the most optimal one. Returns this split index.
-     * Note that the split index is the index of the FIRST element to be in the SECOND group.
-     * --
-     * More specifically, attempts all possible split indices, and picks the split with the least area overlap.
-     * (and smallest area, if area overlap is tied)
+     * Attempts predefined possible splits, and picks the most optimal one. Returns the split index.
      */
     private int chooseSplitIndex(List<Rectangle> rectangles){
         int minimumOverlap = Integer.MAX_VALUE;
@@ -199,6 +194,23 @@ public final class R_tree<T extends GameObject2D> {
             }
         }
         return solutionIndex;
+    }
+
+    /**
+     * Calculates the total margin sum of predefined distributions of the list of SORTED rectangles and returns it.
+     */
+    public int calculateMarginSum(List<Rectangle> rectangles){
+        int marginSum = 0;
+        for(int k = 1; k < (max - 2 * min + 2); k++){
+            int firstGroupAmount = min - 1 + k;
+            List<Rectangle> first = rectangles.subList(0, firstGroupAmount);
+            List<Rectangle> second = rectangles.subList(firstGroupAmount, rectangles.size());
+            Rectangle r1 = Rectangle.createMinimumBounding(first);
+            Rectangle r2 = Rectangle.createMinimumBounding(second);
+            marginSum += r1.perimeter();
+            marginSum += r2.perimeter();
+        }
+        return marginSum;
     }
 
     /**
@@ -244,56 +256,11 @@ public final class R_tree<T extends GameObject2D> {
         return sortedAlongY;
     }
 
-    /**
-     * Calculates the margin sum of all predefined distributions of the list of objects along a given axis.
-     * --
-     * More specifically, creates distributions for k = 1,...,(M - 2*m + 2) exclusive, where for each k
-     * the sorted objects are split into two groups:
-     * - group 1 will contain the first m - 1 + k elements
-     * - group 2 will contain all the other elements
-     * The MBR for both these groups will then be calculated, and the margins added to the result.
-     *
-     * @param rectangles The list of objects. MUST BE SORTED!
-     */
-    public int calculateMarginSum(List<Rectangle> rectangles){
-        int marginSum = 0;
-        for(int k = 1; k < (max - 2 * min + 2); k++){
-            int firstGroupAmount = min - 1 + k;
-            List<Rectangle> first = rectangles.subList(0, firstGroupAmount);
-            List<Rectangle> second = rectangles.subList(firstGroupAmount, rectangles.size());
-            Rectangle r1 = Rectangle.createMinimumBounding(first);
-            Rectangle r2 = Rectangle.createMinimumBounding(second);
-            marginSum += r1.perimeter();
-            marginSum += r2.perimeter();
-        }
-        return marginSum;
-    }
-
-    /**
-     * Selects the optimal Node at the given target depth for insertion of the given Rectangle, and returns it.
-     */
     private Node<T> chooseSubTree(Rectangle rectangle, int targetDepth){
-        Objects.requireNonNull(rectangle);
-        if(targetDepth <= 0 || targetDepth > this.depth){
-            throw new IllegalArgumentException("The max tree depth to search cannot be negative or larger than the tree's height.");
-        }
         return chooseSubTree(root, rectangle, targetDepth, 1);
     }
 
-    /**
-     * Picks the appropriate Node of the R*-tree, at the given target depth, in which to insert the Node/object with
-     * the given Rectangle.
-     * --
-     * To select the most appropriate Node, we use multiple LEXICOGRAPHIC metrics, depending on the current depth.
-     * In each metric, we add the Rectangle to its current MBR (Minimum Bounding Rectangle) to create a new, usually
-     * larger MBR, which we use to rank Nodes.
-     *
-     * @return The appropriate node to insert into, at the provided depth.
-     */
     private Node<T> chooseSubTree(Node<T> current, Rectangle rectangle, int targetDepth, int depth){
-        if(depth < 0 || depth > targetDepth){
-            throw new IllegalArgumentException("Current depth is invalid.");
-        }
         if(depth == targetDepth){
             return current;
         }
@@ -311,16 +278,6 @@ public final class R_tree<T extends GameObject2D> {
         return chooseSubTree(optimalChild, rectangle, targetDepth, depth + 1);
     }
 
-    /***
-     * If the current depth is 1 layer above the tree's leaf layer, 3 metrics will be used to select the best child:
-     * - 1) The sum of the overlaps of the new MBR with all its siblings' MBRs is calculated for each child.
-     *      The child with the smallest overlap sum is selected.
-     * - 2) The area enlargement of the new MBR compared to its original MBR is calculated for each child.
-     *      The child with the smallest area enlargement is selected.
-     * - 3) The total area of the new MBR is calculated for each child.
-     *      The child with the smallest total area is selected.
-     * - 4) If all 3 are tied, the first one where all 3 metrics are tied will be selected.
-     */
     private Node<T> selectOptimalNodeLeafParent(List<Node<T>> nodes, Rectangle rectangle){
         Node<T> solution = nodes.get(0);
         int minimumOverlap = Integer.MAX_VALUE;
@@ -348,16 +305,6 @@ public final class R_tree<T extends GameObject2D> {
         return solution;
     }
 
-    /**
-     * If the current depth is ANYTHING besides leaf level or 1 above the leaf level, 3 metrics will be used:
-     * - 1) The area enlargement of the new MBR compared to its original MBR is calculated for each child.
-     *      The child with the smallest area enlargement is selected.
-     * - 2) The total area of the new MBR is calculated for each child.
-     *      The child with the smallest total area is selected.
-     * - 3) The perimeter of the new MBR is calculated for each child.
-     *      The child with the smallest perimeter is selected.
-     * - 4) If all 3 are tied, the first one where all 3 metrics are tied will be selected.
-     */
     private Node<T> selectOptimalNodeInternal(List<Node<T>> nodes, Rectangle rectangle){
         Node<T> solution = nodes.get(0);
         int minimumEnlargement = Integer.MAX_VALUE;
